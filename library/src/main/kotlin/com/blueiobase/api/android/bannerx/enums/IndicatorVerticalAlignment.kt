9f07cd48cdc1b0239448538b1e538eaac3f95321
IODevBlue <iodevblue@gmail.com>
2023-03-16 16:10:04 +0100

    IndicatorVerticalAlignment.kt: Renamed IndicatorVerticalPosition.kt to IndicatorVerticalAlignment.kt

---
+++ library/src/main/kotlin/com/blueiobase/api/android/bannerx/enums/IndicatorVerticalAlignment.kt
@@ -0,0 +1,13 @@
+package com.blueiobase.api.android.bannerx.enums
+
+/**
+ * Constants representing the vertical location of the `BannerX` indicator widget.
+ * @author IODevBlue
+ * @since 1.0.0
+ */
+enum class IndicatorVerticalAlignment {
+    /** Align indicator widget at the top of `BannerX`. */
+    TOP,
+    /** Align indicator widget at the bottom of `BannerX`. */
+    BOTTOM,
+}
